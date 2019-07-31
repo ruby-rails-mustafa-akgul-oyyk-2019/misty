d402ad50664806f9fff74e8e3d0c37eb0c65615a
coskuntekin <coskuntekin@coskuntekins-MacBook-Pro.local>
2019-07-31 14:28:45 +0300

    Add toaster

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+setTimeout(() => {
+  document.querySelector('.js-toaster').classList.add('hidden');
+}, 3000)
